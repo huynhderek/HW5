6a5d2457bd4bf19fb1f27916047c62b729d7e2ed
Derek Huynh <derekhuynh8791@gmail.com>
2022-03-03 14:46:02 -0800

    Update ex3.js

--- js/ex3.js
+++ js/ex3.js
@@ -1,33 +1,81 @@
-//Declare studentList Array
-const studentList = [
+const studentList=[
+
     {
-        firstName: "Allan",
-        lastName: "Able",
-        scores: [95, 85, 92, 98]
+    
+    firstName:"Allan",
+    
+    lastName:"Able",
+    
+    scores:[95,85,92,98]
+    
     },
+    
     {
-        firstName: "Amy",
-        lastName: "Alexander",
-        scores: [80, 88, 100]
+    
+    firstName:"Amy",
+    
+    lastName:"Alexander",
+    
+    scores:[80,88,100]
+    
     },
+    
     {
-        firstName: "Betty",
-        lastName: "Barns",
-        scores: [70, 80, 90, 100]
+    
+    firstName:"Betty",
+    
+    lastName:"Barns",
+    
+    scores:[70,80,90,100]
+    
     },
+    
     {
-        firstName: "Bob",
-        lastName: "Bones",
-        scores: [75, 85, 95, 85]
+    
+    firstName:"Bob",
+    
+    lastName:"Bones",
+    
+    scores:[75,85,95,85]
+    
     },
+    
     {
-        firstName: "Cindy",
-        lastName: "Chase",
-        scores: [95, 90, 92, 98]
+    
+    firstName:"Cindy",
+    
+    lastName:"Chase",
+    
+    scores:[95,90,92,98]
+    
     },
+    
     {
-        firstName: "Charles",
-        lastName: "Chips",
-        scores: [88, 99, 90]
+    
+    firstName:"Charles",
+    
+    lastName:"Chips",
+    
+    scores:[88,99,90]
+    
     },
-];
+    
+    ]
+    
+    cLastNameResults = studentList.filter((studentLi) => studentLi.lastName.startsWith("C")).map(studentLi=> ({ ...studentLi, minScore: studentLi.scores.reduce(function (p, v) {
+    
+    return ( p < v ? p : v );
+    
+    }),
+    
+    maxScore:studentLi.scores.reduce(function (p, v) {
+    
+    return ( p > v ? p : v );
+    
+    }),
+    
+    avgScore:studentLi.scores.reduce((a, b) => (a + b))/studentLi.scores.length }));
+    
+    console.log(cLastNameResults);
+    
+    
